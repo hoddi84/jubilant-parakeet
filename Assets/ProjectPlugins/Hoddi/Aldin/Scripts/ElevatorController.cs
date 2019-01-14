c4885f1eb144aecf0efe5a7fb0b7833f0141deb3
hoddi84 <hordurh15@ru.is>
2019-01-14 00:53:28 +0000

    Minor changes

--- Assets/ProjectPlugins/Hoddi/Aldin/Scripts/ElevatorController.cs
+++ Assets/ProjectPlugins/Hoddi/Aldin/Scripts/ElevatorController.cs
@@ -161,3 +161,4 @@ public class ElevatorController : MonoBehaviour
         StopElevator();
     }
 }
+
--- Assets/ProjectPlugins/Hoddi/Aldin/Scripts/LightBehavior.cs
+++ Assets/ProjectPlugins/Hoddi/Aldin/Scripts/LightBehavior.cs
@@ -5,7 +5,11 @@
  * - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
 
 using System.Collections;
+using System.Reflection;
 using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class LightBehavior : MonoBehaviour
 {
@@ -15,11 +19,18 @@ public class LightBehavior : MonoBehaviour
     public AudioClip _LightStaticClip;              // Audioclip when the light is turned on. (default)
     public AudioClip _CreepyVoiceClip;              // Audioclip when the lights are shutting down.
 
-    public Light _REFLight;                         // The Light component.
-    public AudioSource _REFLightAudioSrc;           // Audiosource that is attached to the light.
-    public MeshRenderer _REFLightBulbRenderer;      // MeshRenderer of the light bulb.
+    private Light _light;                           // The Light component.
+    private AudioSource _lightAudioSrc;              // Audiosource that is attached to the light.
+    private MeshRenderer _lightRenderer;            // MeshRenderer of the light bulb.
 
     private Coroutine _lightBehaviorCoroutine;      // Coroutine that will run the light behaviour.
+
+    private void Awake()
+    {
+        _light = GetComponentInChildren<Light>();
+        _lightAudioSrc = GetComponentInChildren<AudioSource>();
+        _lightRenderer = GetComponentInChildren<MeshRenderer>();
+    }
 
     private void Start()
     {
@@ -32,12 +43,12 @@ public class LightBehavior : MonoBehaviour
         /*
          * Light becomes completely dark, emits no sound.
          */
-        _REFLightBulbRenderer.material.DisableKeyword("_EMISSION");
-        _REFLight.enabled = false;
-        _REFLightAudioSrc.Stop();
-        _REFLightAudioSrc.loop = false;
-        _REFLightAudioSrc.clip = _LightOffClip;
-        _REFLightAudioSrc.Play();
+        _lightRenderer.material.DisableKeyword("_EMISSION");
+        _light.enabled = false;
+        _lightAudioSrc.Stop();
+        _lightAudioSrc.loop = false;
+        _lightAudioSrc.clip = _LightOffClip;
+        _lightAudioSrc.Play();
 
         if (permanent)
         {
@@ -56,8 +67,8 @@ public class LightBehavior : MonoBehaviour
         /*
          * Light turns on. Sound played.
          */
-        _REFLight.enabled = true;
-        _REFLightBulbRenderer.material.EnableKeyword("_EMISSION");
+        _light.enabled = true;
+        _lightRenderer.material.EnableKeyword("_EMISSION");
         PlaySound(_LightOnClip, false);
     }
 
@@ -66,10 +77,10 @@ public class LightBehavior : MonoBehaviour
         /*
          * Play a sound, with a specified clip and specify if it should loop.
          */
-        _REFLightAudioSrc.Stop();
-        _REFLightAudioSrc.clip = clip;
-        _REFLightAudioSrc.loop = loop;
-        _REFLightAudioSrc.Play();
+        _lightAudioSrc.Stop();
+        _lightAudioSrc.clip = clip;
+        _lightAudioSrc.loop = loop;
+        _lightAudioSrc.Play();
     }
 
     private IEnumerator Behavior()
@@ -98,3 +109,85 @@ public class LightBehavior : MonoBehaviour
         }
     }
 }
+
+#if UNITY_EDITOR
+[CustomEditor(typeof(LightBehavior), true)]
+public class LightBehaviorEditor : Editor
+{
+    SerializedProperty _pLightOnClip;
+    SerializedProperty _pLightOffClip;
+    SerializedProperty _pLightStaticClip;
+    SerializedProperty _pCreepyVoiceClip;
+
+    public void OnEnable()
+    {
+        _pLightOnClip = serializedObject.FindProperty("_LightOnClip");
+        _pLightOffClip = serializedObject.FindProperty("_LightOffClip");
+        _pLightStaticClip = serializedObject.FindProperty("_LightStaticClip");
+        _pCreepyVoiceClip = serializedObject.FindProperty("_CreepyVoiceClip");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        serializedObject.Update();
+
+        LightBehavior t = (LightBehavior)target;
+
+        Color defaultGUIColor = GUI.color;
+
+        GUIStyle smallLabelStyle = new GUIStyle(GUI.skin.box)
+        {
+            fontSize = 7,
+            alignment = TextAnchor.MiddleCenter,
+        };
+
+        GUIStyle boxGroupWithLabelStyle = new GUIStyle(GUI.skin.box)
+        {
+            margin = new RectOffset(5, 5, 10, 5),
+            padding = new RectOffset(5, 5, 12, 5)
+        };
+
+        GUI.color = Color.grey;
+
+        GUIHelper.GroupedBoxFielWithLabel(() =>
+        {
+            GUI.color = defaultGUIColor;
+
+            GUIHelper.GroupedBoxVertical(() =>
+            {
+                GUIHelper.PropertyFieldWithLabel(_pLightOnClip, _pLightOnClip.displayName);
+                GUIHelper.PropertyFieldWithLabel(_pLightOffClip, _pLightOffClip.displayName);
+                GUIHelper.PropertyFieldWithLabel(_pLightStaticClip, _pLightStaticClip.displayName);
+                GUIHelper.PropertyFieldWithLabel(_pCreepyVoiceClip, _pCreepyVoiceClip.displayName);
+            });
+        }, "Assets", smallLabelStyle, boxGroupWithLabelStyle);
+
+        GUI.color = Color.grey;
+
+        GUIHelper.GroupedBoxFielWithLabel(() =>
+        {
+            GUI.color = defaultGUIColor;
+
+            GUIHelper.GroupedBoxHorizontal(() =>
+            {
+                MethodInfo[] methods = target.GetType().GetMethods(BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+
+                for (int i = 0; i < methods.Length; i++)
+                {
+                    object[] customAttributes = methods[i].GetCustomAttributes(typeof(ButtonAttribute), true);
+
+                    if (customAttributes.Length > 0)
+                    {
+                        if (GUILayout.Button(methods[i].Name))
+                        {
+                            ((LightBehavior)target).Invoke(methods[i].Name, 0f);
+                        }
+                    }
+                }
+            });
+        }, "Controls", smallLabelStyle, boxGroupWithLabelStyle);
+
+        serializedObject.ApplyModifiedProperties();
+    }
+}
+#endif
